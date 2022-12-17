62a34029b72a33e6d88db2a1b1f2497710899d57
박규영 <magic9282m@gmail.com>
2022-12-17 15:07:07 +0900

    Section 12: React & Optimization Techniques

---
+++ section12/react-complete-guide/src/App.js
@@ -0,0 +1,31 @@
+import React, { useCallback, useState } from 'react';
+import Button from './components/UI/Button/Button'
+import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  const toggleParagraphHandler =  useCallback(() => {
+    if(allowToggle){
+      setShowParagraph(prevState=>!prevState);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(prevState=>!prevState);
+  }
+
+  console.log('APP RUNNING!');
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Toggle Allow</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
