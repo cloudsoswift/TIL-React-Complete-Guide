5e78d5dbfbb39085ca872e2263b398e843097f6c
박규영 <magic9282m@gmail.com>
2023-01-24 21:41:10 +0900

    Section 19: Diving Deeper Into Redux

---
+++ section19/src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Second Book",
+    description: "This is a first product - amazing!",
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((products) => (
+          <ProductItem
+            key={products.id}
+            id={products.id}
+            title={products.title}
+            price={products.price}
+            description={products.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
---
+++ section19/src/store/cart-actions.js
@@ -0,0 +1,82 @@
+import { cartActions } from "./cart-slice";
+import { uiActions } from "./ui-slice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-http-77b70-default-rtdb.firebaseio.com/cart.json"
+      );
+      if (!response.ok) {
+        throw new Error("Could Not Fetch Cart Data!");
+      }
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart({
+        items: cartData.items || [],
+        totalQuantity: cartData.totalQuantity,
+      }));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Fetching Cart Data Failed!",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cartData) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending Cart Data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-77b70-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cartData.items,
+            totalQuantity: cartData.totalQuantity,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending Cart Data Failed!");
+      }
+    };
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent Cart Data Successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending Cart Data Failed!",
+        })
+      );
+    }
+  };
+};
